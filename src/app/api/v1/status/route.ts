399b63bdd337222255f662eb7c2831800c186759
Bruno Fialho <brugerf@gmail.com>
2025-11-10 09:57:41 -0300

    feat: add Docker Compose config, environments and database.ts

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,6 +1,13 @@
 import { NextResponse } from "next/server";
 
+import database from "@/infra/database";
+
 export async function GET() {
+  const result = await database.query({
+    text: "SELECT 1 + 1 as sum;",
+  });
+  console.log(result.rows);
+
   return NextResponse.json(
     { chave: "são acima da média" },
     { status: 200 }
