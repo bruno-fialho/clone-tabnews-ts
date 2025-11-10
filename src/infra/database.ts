fa4aecd56ccd0ce9439fb9cbd56cccc412edf9e1
Bruno Fialho <brugerf@gmail.com>
2025-11-10 10:12:13 -0300

    feat: implement SSL configuration for database connection

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,5 +1,16 @@
 import { Client, QueryConfig } from "pg";
 
+function getSSLValues() {
+  // If there is a POSTGRES_CA (Certificate Authority) env var, use it for SSL configuration
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV !== "development" && process.env.NODE_ENV !== "test";
+}
+
 async function query(queryObject: QueryConfig) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
@@ -7,16 +18,16 @@ async function query(queryObject: QueryConfig) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
   
-  await client.connect();
-  
   try {
+    await client.connect();
     const result = await client.query(queryObject);
-    
     return result;
   } catch (error) {
     console.error("[database.query]  error:", error);
+    throw error;
   } finally {
     await client.end();
   }
