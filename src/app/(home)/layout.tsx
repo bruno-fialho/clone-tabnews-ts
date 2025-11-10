c4db0d31c3a0bb1d423458a87ee2da4e777e4cc6
Bruno Fialho <brugerf@gmail.com>
2025-11-09 22:07:20 -0300

    feat: add Prettier config

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,16 +1,16 @@
 export const metadata = {
-  title: 'TabNews',
-  description: 'TabNews clone from curso.dev',
-}
+  title: "TabNews",
+  description: "TabNews clone from curso.dev",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  )
+  );
 }
