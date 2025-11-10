6d0c9adaaf458d9385805f140ded3f27ec2dd247
Bruno Fialho <brugerf@gmail.com>
2025-11-09 22:00:10 -0300

    feat: create Home component

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'TabNews',
+  description: 'TabNews clone from curso.dev',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
