cde783f7f41a719cc18316f8f4c14b29b2874410
Bruno Fialho <brugerf@gmail.com>
2025-11-10 15:57:44 -0300

    fix: linting migrations get test

--- src/tests/integration/api/v1/migrations/get.test.ts
+++ src/tests/integration/api/v1/migrations/get.test.ts
@@ -13,7 +13,7 @@ test("GET to /api/v1/migrations should return 200", async () => {
   const response: Response = await fetch(
     "http://localhost:3000/api/v1/migrations",
   );
-  
+
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
