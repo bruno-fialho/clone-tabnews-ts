1375a83ef91d90c72564610dd48e1cd8b5e87b85
Bruno Fialho <brugerf@gmail.com>
2025-11-10 11:57:05 -0300

    feat: add migrations endpoint

---
+++ src/app/api/v1/migrations/route.ts
@@ -0,0 +1,58 @@
+import { NextResponse } from "next/server";
+import migrationsRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import { Client } from "pg";
+
+import { database } from "@/infra/database";
+
+function getDefaultMigrationOptions(dbClient: Client) {
+  return {
+    dbClient: dbClient,
+    dir: resolve(process.cwd(), "src", "infra", "migrations"),
+    direction: "up" as const,
+    dryRun: true,
+    migrationsTable: "pgmigrations",
+    verbose: true,
+  }
+}
+
+export async function GET() {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = getDefaultMigrationOptions(dbClient);
+
+  try {
+    const pendingMigrations = await migrationsRunner(defaultMigrationOptions);
+    return NextResponse.json(pendingMigrations, { status: 200 });
+  } catch (error) {
+    console.error("[migrations GET] error:", error);
+    return NextResponse.json(
+      { error: "Failed to retrieve migrations" },
+      { status: 500 }
+    );
+  } finally {
+    await dbClient.end();
+  }
+}
+
+export async function POST() {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = getDefaultMigrationOptions(dbClient);
+
+  try {
+    const migratedMigrations = await migrationsRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    const status = migratedMigrations.length > 0 ? 201 : 200;
+    return NextResponse.json(migratedMigrations, { status });
+  } catch (error) {
+    console.error("[migrations POST] error:", error);
+    return NextResponse.json(
+      { error: "Failed to run migrations" },
+      { status: 500 }
+    );
+  } finally {
+    await dbClient.end();
+  }
+}
