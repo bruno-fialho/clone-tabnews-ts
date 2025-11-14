f6cb11c013988312cebde2c9e7ca1698f461bcec
Bruno Fialho <brugerf@gmail.com>
2025-11-14 14:54:59 -0300

    feat: implement InternalServerError class

---
+++ src/infra/errors.ts
@@ -0,0 +1,23 @@
+export class InternalServerError extends Error {
+  action: string;
+  statusCode: number;
+
+  constructor({ cause }: { cause?: Error }) {
+    super("Um erro interno não esperado aconteceu.", {
+      cause,
+    });
+
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      statusCode: this.statusCode,
+    };
+  }
+}
